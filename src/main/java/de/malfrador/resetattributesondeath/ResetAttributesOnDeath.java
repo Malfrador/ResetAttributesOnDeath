8992b6913e5a9a71989b426b45a028fc70a91fc5
Malfrador <malfrador@gmail.com>
2024-06-29 02:34:24 +0200

    Initial commit

---
+++ src/main/java/de/malfrador/resetattributesondeath/ResetAttributesOnDeath.java
@@ -0,0 +1,17 @@
+package de.malfrador.resetattributesondeath;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ResetAttributesOnDeath extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
