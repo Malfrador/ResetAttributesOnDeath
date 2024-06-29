7358badcfc22429b0f9b6868df5e0b31cfa3d5a1
Malfrador <malfrador@gmail.com>
2024-06-29 02:38:15 +0200

    Initial commit

--- src/main/java/de/malfrador/resetattributesondeath/ResetAttributesOnDeath.java
+++ src/main/java/de/malfrador/resetattributesondeath/ResetAttributesOnDeath.java
@@ -1,17 +1,35 @@
 package de.malfrador.resetattributesondeath;
 
+import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ResetAttributesOnDeath extends JavaPlugin {
+public final class ResetAttributesOnDeath extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    private void onDeath(PlayerDeathEvent event) {
+        Player player = event.getPlayer();
+        for (Attribute attribute : Attribute.values()) {
+            if (player.getAttribute(attribute) == null) {
+                continue;
+            }
+            for (AttributeModifier modifier : player.getAttribute(attribute).getModifiers()) {
+                if (!(modifier.getKey().getNamespace().equals("minecraft"))) {
+                    continue;
+                }
+                player.getAttribute(attribute).removeModifier(modifier);
+            }
+        }
+
     }
 }
